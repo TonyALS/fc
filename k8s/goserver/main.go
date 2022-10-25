95792878a0dfb10800c6b7aaeca7cd5b4650690a
tonyals <tony_luzsilva@hotmail.com>
2022-10-25 08:31:39 -0300

    feat: add secret example

--- k8s/goserver/main.go
+++ k8s/goserver/main.go
@@ -1,16 +1,24 @@
 package main
 
-import ("net/http"
-		"os"
-		"fmt"
-	)
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(rw, "Hello %s age %s", name, age)
+	})
+
+	http.HandleFunc("/secret", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(rw, "User %s password %s", user, password)
 	})
 	http.ListenAndServe(":8080", nil)
 }
